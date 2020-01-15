2fa6abe8d798d9aad9e02516e7e89a2f8077b114
unknown <parthiban87.tnr@gmail.com>
2020-01-15 19:32:35 +0530

    Init Project Commit

--- src/main/java/com/example/demo/WCController.java
+++ src/main/java/com/example/demo/WCController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
+@RequestMapping("/project")
 public class WCController {
 	
-	@RequestMapping("/test")	
+	@GetMapping("/init")	
 	public String init() {
 		return "Spring World";
 	}
